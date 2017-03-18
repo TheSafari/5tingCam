50f32a7e63f8f4f6d44f443790867a252bf10e4c
Cony Tran <ttpthao.a4cvl@gmail.com>
2017-03-18 14:21:30 +0700

    Add app icon, implement main page UI

---
+++ FightingCam/EditViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EditViewController.swift
+//  FightingCam
+//
+//  Created by Phuong Thao Tran on 3/18/17.
+//  Copyright © 2017 TheSafari. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var image: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.image = image
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FightingCam/HomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  HomeViewController.swift
+//  FightingCam
+//
+//  Created by Phuong Thao Tran on 3/18/17.
+//  Copyright © 2017 TheSafari. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UINavigationControllerDelegate,  UIImagePickerControllerDelegate {
+    
+    var selectedImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onOpenPhotoLibrary(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+        imagePicker.allowsEditing = false
+        self.present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func onTakePhoto(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+        imagePicker.allowsEditing = false
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            selectedImage = image
+            performSegue(withIdentifier: "goToEditScreen", sender: self)
+        } else{
+            print("Something went wrong")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToEditScreen" {
+            if let editVc = segue.destination as? EditViewController {
+                editVc.image = selectedImage!
+            }
+        }
+    }
+}
