8bee3d57a49f837ccc52b5ab5aab9733cc210dee
Cony Tran <ttpthao.a4cvl@gmail.com>
2017-04-01 10:18:59 +0700

    Add spinner while getting data

--- FightingCam/EditViewController.swift
+++ FightingCam/EditViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 import AVFoundation
+import MBProgressHUD
 
 
 class EditViewController: UIViewController {
-
+    
     
     @IBOutlet weak var ivEmoticon: EmoticonImageView!
     
@@ -24,9 +25,12 @@ class EditViewController: UIViewController {
         
         speaker = Speaker()
         speaker?.setMyDelegate(self)
-
+        
         ivEmoticon.image = image
         let imageData = UIImagePNGRepresentation(image!)
+        
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
         DataManager.shareInstance.fetchFaceInfoFromUrl(data: imageData!, completion: { (items) -> (Void) in
             // Handle after fetch to api success
             print("Fetch Success Item = : \(items) ")
@@ -45,29 +49,28 @@ class EditViewController: UIViewController {
                         self.ivEmoticon.addEmoticionFace(face: face, imageFace: faceUiImage!)
                         self.faces.append(face)
                         //get animal sound
+                        
+                        MBProgressHUD.hide(for: self.view, animated: true)
                     }
                 })
-                
             }
-            
         })
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     @IBAction func onBackButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
@@ -113,36 +116,36 @@ class EditViewController: UIViewController {
     }
     
     func saveImage2(){
-            let bgImage = ivEmoticon.image
-            print("width: \(bgImage?.size.width)")
-            print("height: \(bgImage?.size.height)")
+        let bgImage = ivEmoticon.image
+        print("width: \(bgImage?.size.width)")
+        print("height: \(bgImage?.size.height)")
         
-            let newSize = CGSize(width: (bgImage?.size.width)!, height: (bgImage?.size.height)!)  // set this to what you need
+        let newSize = CGSize(width: (bgImage?.size.width)!, height: (bgImage?.size.height)!)  // set this to what you need
         
-            UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        
+        bgImage?.draw(in: CGRect(origin: CGPoint.zero, size: newSize))
+        for index in 0..<faces.count {
+            //let imageView = UIImageView(image: #imageLiteral(resourceName: "Picture1"))
+            let w = faces[index].faceRectangle.Width
+            let h = faces[index].faceRectangle.height
+            let emoticonPos = CGPoint(x: faces[index].faceRectangle.left, y: faces[index].faceRectangle.top)
             
-            bgImage?.draw(in: CGRect(origin: CGPoint.zero, size: newSize))
-            for index in 0..<faces.count {
-                //let imageView = UIImageView(image: #imageLiteral(resourceName: "Picture1"))
-                let w = faces[index].faceRectangle.Width
-                let h = faces[index].faceRectangle.height
-                let emoticonPos = CGPoint(x: faces[index].faceRectangle.left, y: faces[index].faceRectangle.top)
             
-                
-                let faceImage = #imageLiteral(resourceName: "Picture1")
-                faceImage.draw(in: CGRect(origin: emoticonPos, size: CGSize(width: w!, height: h!)))
-                
-            }
-        
-            let lbl = UILabel()
-            lbl.text = "this is my quote"
-            lbl.draw(CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 200)))
-        
-            let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-            UIGraphicsEndImageContext()
+            let faceImage = #imageLiteral(resourceName: "Picture1")
+            faceImage.draw(in: CGRect(origin: emoticonPos, size: CGSize(width: w!, height: h!)))
             
-            UIImageWriteToSavedPhotosAlbum(newImage, nil, nil, nil)
-            print("save done")
+        }
+        
+        let lbl = UILabel()
+        lbl.text = "this is my quote"
+        lbl.draw(CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 200)))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        UIImageWriteToSavedPhotosAlbum(newImage, nil, nil, nil)
+        print("save done")
     }
     
     var bombSoundEffect: AVAudioPlayer!
@@ -151,13 +154,13 @@ class EditViewController: UIViewController {
         if (component.count >= 2) {
             let path = Bundle.main.path(forResource: component[0], ofType:component[1])!
             let url = URL(fileURLWithPath: path)
-        
+            
             do {
                 let sound = try AVAudioPlayer(contentsOf: url)
                 bombSoundEffect = sound
                 sound.play()
             } catch {
-            // couldn't load file :(
+                // couldn't load file :(
             }
         }
     }
---
+++ Pods/Target Support Files/Pods-FightingCam/Pods-FightingCam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FightingCam : NSObject
+@end
+@implementation PodsDummy_Pods_FightingCam
+@end
